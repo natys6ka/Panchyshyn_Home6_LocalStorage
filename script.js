55cdce48d6e825dab8734e32888b58939341d43a
natys6ka <natalkapni.bc@gmail.com>
2020-10-03 15:09:46 +0300

    LoginCheck

--- script.js
+++ script.js
@@ -33,17 +33,53 @@ signupBtn.addEventListener("click", (e) => {
     якщо є то не добавляти його. 
     Всі дані мають валідуватися регулярними виразами. */
 
-
 function setLocalStorageBtn(){
     email = document.getElementById('email').value;
     pass = document.getElementById('password').value;
-    
+    // first = document.getElementById('firstName').value;
+    // second = document.getElementById('lastName').value;
+
+    let person = {
+        first : document.getElementById('firstName').value,
+        last : document.getElementById('lastName').value,
+        email : email,
+        pass : pass
+    }
+    // alert(JSON.stringify(person));
+  
     for(let i=0; i<localStorage.length; i++){
         if (localStorage.key(i) == email)
             alert('This email is already exist');
-        else localStorage.setItem(email, pass);
+        else {
+            localStorage.setItem(email, JSON.stringify(person));
+        } 
     }
 
     // localStorage.clear();
+}
+
+
+    /* 2.	При логінуванні перевіряти чи всі поля заповнені 
+    і чи правильний логін та пароль, 
+    якщо щось не так виводити відповідне повідомлення. 
+    Всі дані беруться з localStorage.
+    3.	Якщо правильний логін та пароль то перейти на блок профайлу.
+    */
+
+function getLocalStorageBtn(){
+    if (localStorage.length < 1) alert('LocalStorage is empty');
+    
+    let logEmail = document.getElementById('logEmail').value;
+    let logPass = document.getElementById('logPass').value;
+    let neww = JSON.parse(localStorage.getItem(logEmail));
+    // alert(neww.pass);
+
+    for(let i=0; i<localStorage.length; i++){
+        if ((localStorage.key(i) == logEmail) & (logPass == neww.pass)) {
+            alert('true'); ///////////////////
+        }
+        else alert('Incorrect email or password');
+    }
+
 
 }
